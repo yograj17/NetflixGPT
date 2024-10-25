0306a3b051db67fc663866e7286afe149a2df413
yograj17 <yograjpatil777@gmail.com>
2024-10-25 10:31:35 +0530

    for mobile resposnsive

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,12 +3,9 @@ import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
   return (
-    <div className="h-24  flex items-center justify-between px-4 lg:px-20 lg:gap-72 md:gap-96 md:pl-40">
-      <img
-        src={LOGO_URL}
-        className="h-16 w-32 md:h-20 md:w-40 lg:h-24 lg:w-48"
-      />
-      <button className="bg-red-600 text-white h-9 w-28 rounded-lg font-semibold text-sm md:text-base lg:text-lg">
+    <div className="h-24  flex  justify-between px-8 items-center  bg-gradient-to-b from-black/100 via-black/70 to-black/0">
+      <img src={LOGO_URL} className="h-14 w-28 cursor-pointer" />
+      <button className="bg-red-600 text-white h-8 w-24  rounded-md text-sm font-semibold cursor-pointer">
         Sign Out
       </button>
     </div>
---
+++ src/utils/optionData.js
@@ -0,0 +1,27 @@
+import { FaTv } from "react-icons/fa6";
+import { MdDownload } from "react-icons/md";
+import { IoTelescope } from "react-icons/io5";
+import { FaSnapchat } from "react-icons/fa6";
+
+export const optionData = [
+  {
+    heading: "Enjoy on your TV",
+    info: "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    logo: <FaTv />,
+  },
+  {
+    heading: "Download your shows to watch offline",
+    info: "Save your favourites easily and always have something to watch.",
+    logo: <MdDownload />,
+  },
+  {
+    heading: "Watch everywhere",
+    info: "Stream unlimited movies and TV shows on your phone, tablet, laptop and TV.",
+    logo: <IoTelescope />,
+  },
+  {
+    heading: "Create profiles for kids",
+    info: "Send kids on adventures with their favourite characters in a space made just for them — free with your membership.",
+    logo: <FaSnapchat />,
+  },
+];
